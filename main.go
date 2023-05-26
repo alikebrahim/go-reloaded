8cdbeda09072e1fd4c996a60d410717dcd21659c
aliebrahim <alikebrabim@me.com>
2023-05-26 16:59:00 +0300

    lexer data struct built && tokens regex defined

--- main.go
+++ main.go
@@ -4,8 +4,68 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
-	"strings"
+	"regexp"
 )
+
+type Token int
+
+const (
+	Modifier Token = iota
+	Number
+	Identifier
+	Punct
+	Whitespace
+	Invalid
+)
+
+type Lexer struct {
+	input     string
+	position  int
+	tokens    []Token
+	tokenVals []string
+}
+
+func NewLexer(input string) *Lexer {
+	return &Lexer{
+		input:     input,
+		position:  0,
+		tokens:    []Token{},
+		tokenVals: []string{},
+	}
+}
+
+func (l *Lexer) Scan() {
+	reIdentifier := regexp.MustCompile(`[a-zA-Z0-9_]\w*`)
+	reModifier := regexp.MustCompile(`\([hex|bin|cap|low|up]+(?:,\s\d)?\)`)
+	rePunct := regexp.MustCompile(`[.,?!:;]+`)
+	reWhitespace := regexp.MustCompile(`\s+`)
+
+	for l.position < len(l.input) {
+		substr := l.input[l.position:]
+
+		if match := reIdentifier.FindStringIndex(substr); match != nil && match[0] == 0 {
+			l.tokens = append(l.tokens, Identifier)
+			l.tokenVals = append(l.tokenVals, reIdentifier.FindString(substr))
+			l.position += match[1]
+		} else if match := reModifier.FindStringIndex(substr); match != nil && match[0] == 0 {
+			l.tokens = append(l.tokens, Modifier)
+			l.tokenVals = append(l.tokenVals, reModifier.FindString(substr))
+			l.position += match[1]
+		} else if match := reWhitespace.FindStringIndex(substr); match != nil && match[0] == 0 {
+			l.tokens = append(l.tokens, Whitespace)
+			l.tokenVals = append(l.tokenVals, reWhitespace.FindString(substr))
+			l.position += match[1]
+		} else if match := rePunct.FindStringIndex(substr); match != nil && match[0] == 0 {
+			l.tokens = append(l.tokens, Punct)
+			l.tokenVals = append(l.tokenVals, rePunct.FindString(substr))
+			l.position += match[1]
+		} else {
+			l.tokens = append(l.tokens, Invalid)
+			l.tokenVals = append(l.tokenVals, string(substr[0]))
+			l.position++
+		}
+	}
+}
 
 func main() {
 	// ensure proper usage
@@ -22,10 +82,4 @@ func main() {
 	if err != nil {
 		fmt.Println("Error opening input file")
 	}
-
-	split_txt := strings.Split(string(reader), " ")
-	for _, item := range split_txt {
-		fmt.Println(item)
-	}
-	fmt.Println(split_txt)
 }
